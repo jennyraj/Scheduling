9aafab7019d44886bbdb8aec68595d8577cac8b4
BEN ARULAN <barulan@meduestmail.com>
2023-07-16 15:31:09 -0400

    added file

---
+++ Core/Appointment/ApptStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Scheduling.Core.Appointment
+{
+    public static class ApptStatus
+    {
+        public const string Scheduled = nameof(Scheduled);
+        public const string Cancelled = nameof(Cancelled);
+    }
+}
